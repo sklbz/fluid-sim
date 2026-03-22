5e4c8174d3b50c38a8d0ae0e616913bbd09e5586
sklbz <sokol.bozanic@tumo.world>
2026-03-22 08:22:45 +0100

    divergence now go to 0

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,52 @@
 pub mod matrix;
 
+use std::{thread::sleep, time::Duration};
+
 use matrix::Matrix;
 
 fn main() {
     let mut grid = FluidGrid::new((10, 10), 1.0);
-    grid.velocities_x.randomize(-1.0..=1.0);
-    grid.velocities_y.randomize(-1.0..=1.0);
+    grid.velocities_x.randomize(-10.0..=10.0);
+    grid.velocities_y.randomize(-10.0..=10.0);
 
-    println!("Divergence:");
-    for x in 0..grid.cell_count.0 {
-        for y in 0..grid.cell_count.1 {
-            print!("{:^5.1}", grid.divergence_at_cell(x, y));
+    display_divergence(&grid);
+    display_pressure(&grid);
+
+    for _ in 0..10 {
+        for _ in 0..50 {
+            println!("\x1b[2J\x1b[H");
+            display_divergence(&grid);
+            display_pressure(&grid);
+
+            grid.pressure_solve();
+            sleep(Duration::from_millis(100));
         }
-        println!();
+        grid.update_velocities();
+        sleep(Duration::from_millis(200));
     }
-    println!("Pressure:");
-    for x in 0..grid.cell_count.0 {
-        for y in 0..grid.cell_count.1 {
-            print!("{:^5.1}", grid.get_pressure(x, y));
-        }
-        println!();
-    }
-
-    grid.pressure_solve();
-
-    println!("Divergence:");
-    for x in 0..grid.cell_count.0 {
-        for y in 0..grid.cell_count.1 {
-            print!("{:^5.1}", grid.divergence_at_cell(x, y));
-        }
-        println!();
-    }
-    println!("Pressure:");
-    for x in 0..grid.cell_count.0 {
-        for y in 0..grid.cell_count.1 {
-            print!("{:^5.1}", grid.get_pressure(x, y));
-        }
-        println!();
-    }
-
-    grid.pressure_solve();
-
+}
+fn display_divergence(grid: &FluidGrid) {
     println!("Divergence:");
     for x in 0..grid.cell_count.0 {
         for y in 0..grid.cell_count.1 {
             let div = grid.divergence_at_cell(x, y);
-            if div > 0.0 {
-                print!("\x1b[31mRouge\x1b[0m");
+            if div.abs() > 1.0 {
+                if div > 0.0 {
+                    print!("\x1b[31m");
+                } else {
+                    print!("\x1b[34m");
+                }
             } else {
-                print!("\x1b[31mBleu\x1b[0m");
+                print!("\x1b[32m");
             }
             print!("{:^5.1}", div);
-        }
             print!("\x1b[0m");
         }
         println!();
     }
-    println!("Pressure:");
-    for x in 0..grid.cell_count.0 {
-        for y in 0..grid.cell_count.1 {
-            print!("{:^5.1}", grid.get_pressure(x, y));
-        }
-        println!();
-    }
+}
 
-    grid.pressure_solve();
-
-    println!("Divergence:");
-    for x in 0..grid.cell_count.0 {
-        for y in 0..grid.cell_count.1 {
-            print!("{:^5.1}", grid.divergence_at_cell(x, y));
-        }
-        println!();
-    }
+fn display_pressure(grid: &FluidGrid) {
     println!("Pressure:");
     for x in 0..grid.cell_count.0 {
         for y in 0..grid.cell_count.1 {
@@ -128,9 +102,17 @@ impl FluidGrid {
 
     pub fn pressure_solve_cell(&mut self, x: u32, y: u32) {
         let top_pressure = self.get_pressure(x, y + 1);
-        let left_pressure = self.get_pressure(x, y);
+        let left_pressure = if x == 0 {
+            0.0
+        } else {
+            self.get_pressure(x - 1, y)
+        };
         let right_pressure = self.get_pressure(x + 1, y);
-        let bottom_pressure = self.get_pressure(x, y);
+        let bottom_pressure = if y == 0 {
+            0.0
+        } else {
+            self.get_pressure(x, y - 1)
+        };
 
         let top_velocity = self.velocities_y[(x, y + 1)];
         let left_velocity = self.velocities_x[(x, y)];
@@ -160,16 +142,23 @@ impl FluidGrid {
         for x in 0..self.velocities_x.width() {
             for y in 0..self.velocities_x.height() {
                 let pressure_right = self.get_pressure(x, y);
-                let pressure_left = self.get_pressure(x - 1, y);
+                let pressure_left = x
+                    .checked_sub(1)
+                    .map(|px| self.get_pressure(px, y))
+                    .unwrap_or(0.0);
                 self.velocities_x[(x, y)] -= k * (pressure_right - pressure_left);
             }
         }
 
         // ---- Vertical -------------
-        for x in 0..self.velocities_y.width() {
+        for x in 0..self.velocities_y.width() - 1 {
+            // Pourquoi ? Je ne sais pas
             for y in 0..self.velocities_y.height() {
                 let pressure_top = self.get_pressure(x, y);
-                let pressure_bottom = self.get_pressure(x, y - 1);
+                let pressure_bottom = y
+                    .checked_sub(1)
+                    .map(|py| self.get_pressure(x, py))
+                    .unwrap_or(0.0);
                 self.velocities_y[(x, y)] -= k * (pressure_top - pressure_bottom);
             }
         }
